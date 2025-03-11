869b18c9050dcad833b721a5721112c16a1a2e06
Shahzil Shahzad <silentboys1117@gmail.com>
2025-03-11 16:45:47 +0500

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head';
 import Image from 'next/image';
-import Navbar from "@Components/Navbar"
+import Navbar from "@/Components/Navbar";
 const page = () => {
   return (
     <>
