a8c920c83e6125adf42d8670d61c4d96c50a30bd
Shahzil Shahzad <silentboys1117@gmail.com>
2025-03-11 17:08:40 +0500

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,12 +1,13 @@
 import Link from "next/link"
+import Image from "next/image"
 const Navbar = () => {
   return (
       <nav className='h-20 w-full flex justify-between items-center sticky z-10 top-0 bg-purple-400 text-white px-8 py-2'>
         <div className="logo">
-            <img src="/nuthatch.png" className='border-white'  width={70}
-          height={37} alt="" />
+            <Image src="/nuthatch.png" className='border-white'  width={70}
+          height={37} alt="Logo" />
         </div>
-        <ul className='flex justify-evenly items-center gap-5'>
+        <ul className='flex justify-evenly items-center gap-5 text-black'>
             <li><Link href = '/' >Home</Link></li>
             <li><Link href = '/about' >About</Link></li>
             <li><Link href = '/contact' >Contact</Link></li>
